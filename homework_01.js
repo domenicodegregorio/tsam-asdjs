122d2e6dd498a3ace0fe03b0126940be7fd9da6d
Domenico De Gregorio <domenicodegregorio@outlook.it>
2015-11-01 15:38:28 +0100

    ..

--- homework_01.js
+++ homework_01.js
@@ -6,10 +6,32 @@ function ex_1_I(a) {
     return sum;
 }
 
-function ex_5_I(a) {
+function sumAllR(a) {
+    if (a.length == 0) { 
+        return 0;
+    } else { 
+        return a[0] + sumAllR(a.slice(1)); 
+    }
+}
+function ex_1_R (a) {
+    return sumAllR (a);
+}
+
+function ex_3_I(a) {
     sum = 0;
     for (i = 0; i < a.length; i++) {
     sum += a[i];
     }
     return sum / a.length;
+}
+
+function mediaR(a) {
+    if (a.length == 0) { 
+        return 0;
+    } else { 
+        return a[0] + mediaR(a.slice(1)); 
+    }
+}
+function ex_3_R (a) {
+    return (mediaR(a) / a.length);
 }
