0a0c9f0b752a4846154ba7c9e3d9bf2554af1efc
Domenico De Gregorio <domenicodegregorio@outlook.it>
2015-11-17 14:59:15 +0100

    17nov

--- homework_01.js
+++ homework_01.js
@@ -1,3 +1,9 @@
+/*
+Esercizio 1
+Dato un array di interi, restituire la loro somma fino a che non viene ritrovato un valore
+negativo
+*/
+
 function ex_1_I(a) {
     sum = 0;
     for (i = 0; a[i] > -1; i++) {
@@ -18,6 +24,10 @@ function ex_1_R(x) {
     return sumwhilenegR(x);
 }
 
+/*
+Esercizio 2
+Dato un numero n, restituire la somma dei primi n numeri interi positivi dispari
+*/
 
 function sumOdd (n) {
     res = 0;
@@ -41,6 +51,10 @@ function ex_2_R (n) {
     return sumOddR(n);
 }
 
+/*
+Esercizio 3
+Dato un array di 10 elementi, calcolarne la media
+*/
 
 function ex_3_I(a) {
     sum = 0;
@@ -60,6 +74,13 @@ function mediaR(a) {
 function ex_3_R (a) {
     return (mediaR(a) / a.length);
 }
+
+/*
+Esercizio 4
+Dato un intervallo [a, b] con a e b due interi positivi, restituire la somma di tutti i numeri
+compresi allÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ÃƒÂ¢Ã¢â‚¬Å¾Ã‚Â¢interno dellÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ÃƒÂ¢Ã¢â‚¬Å¾Ã‚Â¢intervallo, estremi inclusi. Nel caso che b fosse minore di a,
+calcolare la somma nellÃƒÆ’Ã‚Â¢ÃƒÂ¢Ã¢â‚¬Å¡Ã‚Â¬ÃƒÂ¢Ã¢â‚¬Å¾Ã‚Â¢intervallo [b,a]
+*/
 
 function sumIntervalW(a, b) {
     var tot = 0;
@@ -104,6 +125,13 @@ function ex_4_I(x, y) {
 function ex_4_R(x, y) {
     return sumIntervalR(x, y);
 }
+
+/*
+Esercizio 5
+Si calcoli il prodotto di due numeri a, b maggiori o uguali a zero, tramite lÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢utilizzo del solo
+operatore somma.
+*/
+
 function moltiplica (x, y)
           {
             var i;
@@ -120,6 +148,16 @@ function ex_5_I(a,b) {
         return moltiplica (a,b);
 }
 
+function ex_5_R(a,b){
+	return (b>0)?a+ex_5_R(a,--b):0;
+}
+
+/*
+Esercizio 6
+Si calcoli la divisione e il resto della divisione tra due numeri a, b maggiori a zero, tramite
+lÃƒÂ¢Ã¢â€šÂ¬Ã¢â€žÂ¢utilizzo dei soli operatori somma e sottrazione.
+*/
+
 function dividi (x, y)
         {
             var z = 0;
@@ -135,10 +173,25 @@ function dividi (x, y)
         }
 
         //per Tiziano
+
 function ex_6_I(a,b) {
     return dividi(a,b);
 }
 
+
+function divR(a,b,q){
+    return (a-b>=0)?divR(a-b,b,++q):[q,a];
+}
+
+function ex_6_R(a,b){
+    return divR(a,b,0);
+}
+
+/*
+Esercizio 7
+Si calcoli la potenza (x^y) di due numeri x y maggiori o uguali a zero, tramite lÃ¢â‚¬â„¢utilizzo dei soli
+operatori somma, sottrazione e della funzione mult.
+*/
 
 function pot (x, y)
         {
@@ -158,6 +211,34 @@ function ex_7_I (a,b) {
         return pot(a,b);
 }
 
+function ex_7_R(a,b){
+    return (b==0)?1:ex_5_R(a,ex_7_R(a,b-1));
+}
+
+/*
+Esercizio 8
+Dato un array contenente n^2 elementi, scrivere un algoritmo che permetta di inserire tutti gli
+oggetti in un array bidimensionale n x n.
+*/
 
 
+/*
+Esercizio 9
+Dato una lista di elementi, scrivere un algoritmo che permetta di invertire lâ€™ordine degli
+elementi.
+*/
+
+
+/*
+Esercizio 10
+Dati due interi a, n maggiori di 0, scrivere un algoritmo che crea un lista di n elementi
+contenenti a.
+*/
+
+
+/*
+Esercizio 11
+Data una lista di interi A, si riordini gli elementi della lista in modo tale che tutti gli elementi
+dispari precedano nello stesso ordine tutti gli elementi pari.
+*/
 
