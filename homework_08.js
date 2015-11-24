d94152a328af581111949f47d5bec32780eafe39
Domenico De Gregorio <domenicodegregorio@outlook.it>
2015-11-24 17:19:33 +0100

    ex_8

---
+++ homework_08.js
@@ -0,0 +1,35 @@
+var mergeSortRec = function (array){
+    var length = array.length;
+    if (length === 1) {
+        return array;
+    }
+    var mid = Math.floor(length / 2),
+        left = array.slice (0, mid),
+        right = array.slice (mid, length);
+    return merge (mergeSortRec(left), mergeSortRec(right));
+};
+var merge = function (left, right) {
+    var result = [],
+    il = 0,
+    ir = 0;
+while(il < left.length && ir < right.length) {
+    if(left[il] < right[ir]) {
+        result.push(left[il++]);
+    } else{
+        result.push(right[ir++]);
+    }
+}
+while (il < left.length){
+    result.push(left[il++]);
+}
+while (ir < right.length){
+    result.push(right[ir++]);
+}
+return result;
+}
+function ex_8 (x) {
+    return mergeSortRec (x);
+}
+
+
+
